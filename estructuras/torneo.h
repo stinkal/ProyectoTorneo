812d688cb8143b4712ff0bf740e94e739f4995fe
stinkal <115372272+stinkal@users.noreply.github.com>
2025-11-02 21:49:37 -0600

    funcionalidad completo para torneos

--- estructuras/torneo.h
+++ estructuras/torneo.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <unordered_set>
 #include "../modelos/equipo.h"
 #include "../modelos/grupo.h"
 #include "../modelos/partido.h"
@@ -20,17 +21,44 @@ private:
     mutable std::mt19937 rng;  //  aleatorio
     std::vector<Equipo*> getClasificados() const;
     void limpiarGrupos();
+    static bool esPotenciaDe2(int n);
+    std::unordered_set<Partido*> partidosEncolados;
+    void encolarPartidosArbol();
+    Partido* partidoTercerLugar;
 
 public:
     Torneo();
     ~Torneo();   //  destructor
 
-    bool agregarEquipos(Equipo* e);
+    bool agregarEquipo(Equipo* e);
     bool eliminarEquipo(const std::string& nombre);
     std::vector<std::string> getNombresEquipos() const;
 
     void configurarTorneo(bool modoGrupo);
+    bool esModalidadGrupo() const;
+    void generarGruposAutomatico();
+    void generarCalendarioGrupos();
 
+    bool colaVacia() const;
+    int colaTamano() const;
+    Partido* jugarPartidoSiguiente();
+    bool registrarResultadoEnPartido(Partido* p, int golesLocal, int golesVisitante, bool esEliminatoria = false);
+
+    std::vector<std::string> obtenerResumenGrupos() const;
+    bool generarLlavesDesdeGrupos();
+    bool generarLlavesDirectas(const std::vector<Equipo*>& equiposClasificados);
+    Partido* jugarLlaveSiguiente();
+    std::vector<std::string> obtenerBracket() const;
+    std::string obtenerCampeon() const;
+    Partido* getPartidoTercerLugar() const { return partidoTercerLugar; }
+    void generarTercerLugar();
+
+    bool esPartidoDeEliminatoria(Partido* p) const;
+
+    bool contieneEquipo(const std::string& nombre) const;
+    std::string generarReporteTexto() const;
+    bool guardarEstado(const std::string& archivo) const;
+    bool cargarEstado(const std::string& archivo);
 
 };
 
